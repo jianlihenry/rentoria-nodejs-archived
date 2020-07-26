45d23f46c3d60c3760cb6a1a116f504ec125da10
Henry L <jianlisword@gmail.com>
2020-07-25 17:52:40 -0700

    add express-pino-logger and pino-pretty

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express'),
-  bodyparser = require("body-parser"),
+  bodyparser = require('body-parser'),
+  mongoose = require('mongoose'),
+  pino = require('express-pino-logger')(),
   port = process.env.PORT || 3000,
   app = express();
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
+app.use(pino);
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/rentoria');
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect('mongodb://localhost:27017/rentoria', { useNewUrlParser: true });
 
 const propertyController = require('./api/controllers/property_controller.js');
 propertyController(app);
